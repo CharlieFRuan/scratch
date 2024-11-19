d28fbeef63e66fe9e6ac4d101755cdcc9b7dba17
Charlie Ruan <53290280+CharlieFRuan@users.noreply.github.com>
2024-11-19 06:56:49 +0000

    Add fp8

--- triton_workspace/persitent_matmul_bench/script.sh
+++ triton_workspace/persitent_matmul_bench/script.sh
@@ -1,9 +1,19 @@
 # Run 8K fp16
 python 09-persistent-matmul.py --prec fp16 --K_range 512 8192 --K_step 512 --MN-size 8192
-mv matmul.hatchet matmul_M\=N\=8K.hatchet
-proton-viewer -m "tflop16/s" matmul_M\=N\=8K.hatchet > matmul_M\=N\=8K_output.txt
+mv matmul.hatchet matmul_M\=N\=8K_fp16.hatchet
+proton-viewer -m "tflop16/s" matmul_M\=N\=8K_fp16.hatchet > matmul_M\=N\=8K_output_fp16.txt
 
 # Run 4K fp16
 python 09-persistent-matmul.py --prec fp16 --K_range 256 4096 --K_step 256 --MN-size 4096
-mv matmul.hatchet matmul_M\=N\=4K.hatchet
-proton-viewer -m "tflop16/s" matmul_M\=N\=4K.hatchet > matmul_M\=N\=4K_output.txt
+mv matmul.hatchet matmul_M\=N\=4K_fp16.hatchet
+proton-viewer -m "tflop16/s" matmul_M\=N\=4K_fp16.hatchet > matmul_M\=N\=4K_output_fp16.txt
+
+# Run 8K fp8
+python 09-persistent-matmul.py --prec fp8 --K_range 512 8192 --K_step 512 --MN-size 8192
+mv matmul.hatchet matmul_M\=N\=8K_fp8.hatchet
+proton-viewer -m "tflop8/s" matmul_M\=N\=8K_fp8.hatchet > matmul_M\=N\=8K_output_fp8.txt
+
+# Run 4K fp8
+python 09-persistent-matmul.py --prec fp8 --K_range 256 4096 --K_step 256 --MN-size 4096
+mv matmul.hatchet matmul_M\=N\=4K_fp8.hatchet
+proton-viewer -m "tflop8/s" matmul_M\=N\=4K_fp8.hatchet > matmul_M\=N\=4K_output_fp8.txt
